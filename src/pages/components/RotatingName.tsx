ed02842d48913585916db09324f573ae605c68fd
HuemaYilmaz <huema.yilmaz@gmail.com>
2024-07-24 21:19:06 +0200

    Splitting first page into 2

---
+++ src/pages/components/RotatingName.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '@/styles/RotatingName.module.css';
+
+const RotatingName: React.FC = () => {
+    return (
+        <>
+            <h1 className={styles.futuristicH1}>Stylish Websites</h1>
+        </>
+    );
+};
+
+export default RotatingName;
