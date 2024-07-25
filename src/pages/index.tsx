858cf2585a747dd4674196e726fa545ee763e556
Yusuf Uzun <uzefun38@gmail.com>
2024-07-25 23:46:35 +0200

    Information

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import styles from '@/styles/Home.module.css';
 import RotatingName from './components/RotatingName';
 import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
 import AnimatedSquare from './components/AnimatedCircles';
+import Information from './components/Information';
 
 export default function Home() {
   return (
@@ -18,8 +19,13 @@ export default function Home() {
             </div>
           </div>
         </Parallax>
-        <Parallax speed={50}> {/* Geschwindigkeit angepasst für visuellen Effekt */}
-          //TODO
+        <Parallax speed={50}>
+          <div className={styles.appBody}>
+            <div className={styles.containerSecondPage}>
+              <Information />
+            </div>
+
+          </div>
         </Parallax>  
       </ParallaxProvider>
     </div>
