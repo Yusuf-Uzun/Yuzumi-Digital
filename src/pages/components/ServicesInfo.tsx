d99203b0c6dbda2513712c961c9a0141d76d81ea
yusizn <uzefun38@gmail.com>
2024-07-28 15:11:11 +0200

    wip

--- src/pages/components/ServicesInfo.tsx
+++ src/pages/components/ServicesInfo.tsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { motion } from 'framer-motion';
+import styles from '@/styles/Information.module.css';
 
 export default function ServicesInfo(){
 
   return (
-    <>
-        <motion.h1 style={{ color: "#f0eada" }}>GAMING</motion.h1>
-    </>
-  );
+    <motion.div>
+      <motion.h1 style={{ color: "#f0eada" }}>GAMING</motion.h1>
+    </motion.div>
+);
+  //   <motion.div
+  //     className={styles.counterContainer}
+  //     initial={{ opacity: 0, y: 50 }}
+  //     transition={{ duration: 0.5 }}>
+  //       <motion.h1 style={{ color: "#f0eada" }}>GAMING</motion.h1>
+  //   </motion.div>
+  // );
 };
